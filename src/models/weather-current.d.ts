a47d96e4f5ed26a11f01708f15be2d7e75b69c84
Jonas Schubert <jonas.schubert.projects@web.de>
2019-11-14 21:23:40 +0100

    updates WeatherCurrent

--- src/models/weather-current.d.ts
+++ src/models/weather-current.d.ts
@@ -2,6 +2,7 @@ import { Clouds } from "@/models/clouds";
 import { Coordinates } from "@/models/coordinates";
 import { Main } from "@/models/main";
 import { Sys } from "@/models/sys";
+import { IWeatherCondition } from "@/models/weather-condition";
 import { WeatherPart } from "@/models/weather-part";
 import { Wind } from "@/models/wind";
 
@@ -20,5 +21,5 @@ export interface WeatherCurrent {
     id: number;
     name: string;
     cod: number;
-    weatherCondition: WeatherCondition;
+    weatherCondition: IWeatherCondition;
 }
