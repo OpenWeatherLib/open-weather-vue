47459148ff11d6ed3db6725f5eabb2280696815a
Jonas Schubert <jonas.schubert.projects@web.de>
2019-11-14 20:35:27 +0100

    adds initial example view for UvIndex

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,59 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark></v-app-bar>
-    <v-content></v-content>
+    <v-content style="margin: 0.5rem;">
+      <v-card style="height: calc(100vh - 1rem);">
+        <v-card-title class="text-center justify-center py-6">
+          <h1 class="font-weight-bold display-3 basil--text">OpenWeather</h1>
+        </v-card-title>
+
+        <v-tabs v-model="tab" background-color="deep-purple accent-4" center-active dark>
+          <v-tab key="uv-index">UvIndex</v-tab>
+        </v-tabs>
+
+        <v-tabs-items v-model="tab">
+          <v-container class="fill-height" fluid>
+            <v-row align="center" justify="center">
+              <v-col cols="12" sm="8" md="4">
+                <v-tab-item key="uv-index">
+                  <UvIndexComponent />
+                </v-tab-item>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-tabs-items>
+      </v-card>
+    </v-content>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import Component from "vue-class-component";
+import UvIndexComponent from "@/views/UvIndex.vue";
 
-export default Vue.extend({
-  name: "App"
-});
+@Component({
+  components: { UvIndexComponent },
+  data: () => ({
+    tab: "uv-index"
+  })
+})
+export default class App extends Vue {
+  constructor() {
+    super();
+  }
+}
 </script>
+
+<style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.3s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
+</style>
---
+++ src/views/UvIndex.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-card class="mx-auto" max-width="344" outlined>
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="overline mb-4">UV Index</div>
+        <v-list-item-title class="headline mb-1">{{ uvIndex | valueString }}</v-list-item-title>
+        <v-list-item-subtitle>{{ uvIndex | coordinatesString }}</v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar tile size="80" color="primary">
+        <v-img src="@/assets/uv_index.png"></v-img>
+      </v-list-item-avatar>
+    </v-list-item>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import { UvIndex } from "@/models";
+
+@Component({
+  components: {},
+  computed: {
+    uvIndex(): UvIndex {
+      return this.$store.getters["uvIndexModule/uvIndex"];
+    }
+  },
+  filters: {
+    coordinatesString(uvIndex: UvIndex) {
+      return !!uvIndex
+        ? `Coordinates: Lat: ${uvIndex.lat}, Lon: ${uvIndex.lon}`
+        : "n.a.";
+    },
+    valueString(uvIndex: UvIndex) {
+      return !!uvIndex ? uvIndex.value : "n.a.";
+    }
+  }
+})
+export default class UvIndexComponent extends Vue {
+  constructor() {
+    super();
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
